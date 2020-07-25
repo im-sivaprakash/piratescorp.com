f2ddb43344c341cad403ab523e017671bf029efe
Sivaprakash22 <sivaprakash138@gmail.com>
2020-07-25 12:53:27 +0530

    slider need to fix

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,7 +1,4 @@
 import React from 'react';
-import OwlCarousel from 'react-owl-carousel';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-import 'owl.carousel/dist/assets/owl.theme.default.css';
 import client1 from '../img/clients/chillstudies.JPG';
 import client2 from '../img/clients/srimurugansculpture.jpeg';
 
@@ -13,67 +10,57 @@ const Testmonials = () => {
             <section id="testimonials">
                 <h3 className="section-heading">What our clients says..</h3>
 
-                <OwlCarousel
-                    loop
-                    items={1}
-                    nav
-                    dots={true}
-                    autoplay={true}
-                >
-                    <div className="items">
 
-                        <div className="container">
 
-                            <div className="testimonial-row">
 
-                                <div className="testimonial-pic">
-                                    <div className="person-bg">
-                                        <img className="person" src={client1} alt="chillstudies" />
-                                    </div>
-                                </div>
+                <div className="container">
 
-                                <div className="testimonial-review">
+                    <div className="testimonial-row">
 
-                                    <p className="review">Vom Rechte, das mit Recht; denn alles, was ihr Sünde, Zerstörung, kurz
+                        <div className="testimonial-pic">
+                            <div className="person-bg">
+                                <img className="person" src={client1} alt="chillstudies" />
+                            </div>
+                        </div>
+
+                        <div className="testimonial-review">
+
+                            <p className="review">Vom Rechte, das mit Recht; denn alles, was ihr Sünde, Zerstörung, kurz
               das Böse will und stets das Gute schafft.   das Böse will und stets das Gute schafft.</p>
 
-                                    <div className="client-details">
-                                        <p className="client-name">R.santhi</p>
-                                        <a href="/chillstudies.com" className="website">chillstudies.com</a>
-                                    </div>
-
-                                </div>
+                            <div className="client-details">
+                                <p className="client-name">R.santhi</p>
+                                <a href="/chillstudies.com" className="website">chillstudies.com</a>
                             </div>
+
                         </div>
                     </div>
-                    <div className="items">
 
-                        <div className="container">
 
-                            <div className="testimonial-row">
+                    <div className="testimonial-row">
 
-                                <div className="testimonial-pic">
-                                    <div className="person-bg">
-                                        <img className="person" src={client2} alt="chillstudies" />
-                                    </div>
-                                </div>
+                        <div className="testimonial-pic">
+                            <div className="person-bg">
+                                <img className="person" src={client2} alt="chillstudies" />
+                            </div>
+                        </div>
 
-                                <div className="testimonial-review">
+                        <div className="testimonial-review">
 
-                                    <p className="review">Vom Rechte, das mit Recht; denn alles, was ihr Sünde, Zerstörung, kurz
+                            <p className="review">Vom Rechte, das mit Recht; denn alles, was ihr Sünde, Zerstörung, kurz
                                     das Böse will und stets das Gute schafft.   das Böse will und stets das Gute schafft.</p>
 
-                                    <div className="client-details">
-                                        <p className="client-name">V.Senthamizhselvan</p>
-                                        <a href="/srimurugansculpture.com" className="website">srimurugansculpture.com</a>
-                                    </div>
-
-                                </div>
+                            <div className="client-details">
+                                <p className="client-name">V.Senthamizhselvan</p>
+                                <a href="/srimurugansculpture.com" className="website">srimurugansculpture.com</a>
                             </div>
+
                         </div>
                     </div>
+                </div>
 
-                </OwlCarousel>
+
+
 
             </section>
         </React.Fragment>
