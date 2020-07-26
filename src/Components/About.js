02a23a8d01bdbfb4204d11d11719008322d574aa
Sivaprakash22 <sivaprakash138@gmail.com>
2020-07-26 19:54:23 +0530

    fix

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,29 +1,30 @@
 import React from 'react';
 
-const About = () =>{
-  return(
-      <React.Fragment>
+const About = () => {
+  return (
+    <React.Fragment>
       <section id="about">
-      <div className="container">
+        <div className="container">
           <div className="about-row" >
             <div className="logo-box">logo
           </div>
-          <div className="about-info">
-            <h3 className="about-heading">About Us</h3>
-            <p id="about-text">Commodo et cillum sunt fugiat dolore fugiat dolor proident tempor in ipsum. Id officia eu consequat amet duis nisi aute aliquip velit aliquip velit. 
-            Qui nostrud labore consectetur magna magna sit commodo officia ad. Sunt sint elit reprehenderit aliquip fugiat fugiat labore cupidatat ex fugiat. 
-            Dolore sit duis proident qui et magna ad ullamco sunt est irure est occaecat.</p>
-            <div className="Social-media-holder">
-              <a className="soc-round" href="/insta"><i className="fab fa-whatsapp"></i></a>
-              <a className="soc-round" href="/insta"><i className="fab fa-instagram"></i></a>
-              <a className="soc-round" href="/insta"><i className="fab fa-youtube"></i></a>
-              <a className="soc-round" href="/insta"><i className="fab fa-linkedin-in"></i></a>
+            <div className="about-info">
+              <h3 className="about-heading">About Us</h3>
+              <p id="about-text">We provide a reliable and affordable platform for engaging business
+               applications. We have mainly enabled web hosting services to customers by offering 
+               different packages with more fast and secure cPanel.
+              </p>
+              <div className="Social-media-holder">
+                <a className="soc-round" href="/insta"><i className="fab fa-whatsapp"></i></a>
+                <a className="soc-round" href="/insta"><i className="fab fa-instagram"></i></a>
+                <a className="soc-round" href="/insta"><i className="fab fa-youtube"></i></a>
+                <a className="soc-round" href="/insta"><i className="fab fa-linkedin-in"></i></a>
+              </div>
             </div>
+          </div>
         </div>
-      </div>
-      </div>
       </section>
-      </React.Fragment>
+    </React.Fragment>
   )
 }
 
