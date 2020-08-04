ac3bc3fe2366079928c1a5efa35317ac977f3046
Sivaprakash22 <sivaprakash138@gmail.com>
2020-08-04 15:59:27 +0530

    css fixed

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,67 +1,79 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <React.Fragment>
-            <footer id="footer">
-            
+  return (
+    <React.Fragment>
+      <footer id="footer">
+        <div className="footer-bg">
+          <div className="container">
+            <div className="footer-row">
+              <div className="footer-sec">
+                <div className="footer-logo">logo</div>
+                <p className="logo-text">We make the best</p>
+              </div>
 
-            <div className="footer-bg">
+              <div className="footer-sec">
+                <h4 className="footer-heading">Service</h4>
+                <ul className="footer-list">
+                  <li>
+                    <a href="/test">Web hosting</a>
+                  </li>
+                  <li>
+                    <a href="/test">Domain Registeration</a>
+                  </li>
+                  <li>
+                    <a href="/test">Website development</a>
+                  </li>
+                  <li>
+                    <a href="/test">Graphic / web Designing</a>
+                  </li>
+                </ul>
+              </div>
 
-                    <div className="container">
+              <div className="footer-sec">
+                <h4 className="footer-heading">company</h4>
+                <ul className="footer-list">
+                  <li>
+                    <a href="/test">About</a>
+                  </li>
+                  <li>
+                    <a href="/tos">terms of service</a>
+                  </li>
+                  <li>
+                    <a href="/test">privacy policy</a>
+                  </li>
+                </ul>
+              </div>
 
-                    <div className="footer-row">
-
-
-                        <div className="footer-sec">
-                            <div className="footer-logo">logo</div>
-                            <p className="logo-text">We make the best</p>
-                        </div>
-
-
-                        <div className="footer-sec">
-                            <h4 className="footer-heading">Service</h4>
-                            <ul className="footer-list">
-                                <li><a href="/test">Web hosting</a></li>
-                                <li><a href="/test">Domain Registeration</a></li>
-                                <li><a href="/test">Website development</a></li>
-                                <li><a href="/test">Graphic / web Designing</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="footer-sec">
-                            <h4 className="footer-heading">company</h4>
-                            <ul className="footer-list">
-                                <li><a href="/test">About</a></li>
-                                <li><a href="/test">terms of service</a></li>
-                                <li><a href="/test">privacy policy</a></li>
-                            </ul>
-                        </div>
-
-                        <div className="footer-sec">
-                            <h4 className="footer-heading">Support</h4>
-                            <ul className="footer-list">
-                                <li><a href="/test"><i className="fas fa-phone-alt"></i>+91 98407 93240</a></li>
-                                <li><a href="/test"><i className="fas fa-envelope-open"></i>support@piratescorp.com</a></li>
-
-                            </ul>
-                        </div>
-
-                    </div>
-
-
-                    </div>
-
-
+              <div className="footer-sec">
+                <h4 className="footer-heading">Support</h4>
+                <ul className="footer-list">
+                  <li>
+                    <a href="/test">
+                      <i className="fas fa-phone-alt"></i>+91 98407 93240
+                    </a>
+                  </li>
+                  <li>
+                    <a href="/test">
+                      <i className="fas fa-envelope-open"></i>
+                      support@piratescorp.com
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
+          </div>
+        </div>
 
-
-            <div className="copyrigths">
-                <p id="copyrigths-text"> Pirates &copy; {new Date().getFullYear()} </p>
-            </div>
-            </footer>
-        </React.Fragment>
-    )
-}
+        <div className="copyrigths">
+          <p id="copyrigths-text">
+            {" "}
+            Pirates &copy; {new Date().getFullYear()}{" "}
+          </p>
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+};
 
 export default Footer;
